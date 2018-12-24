8b4a99ca2283560c69161419132aede8e13cf6f0
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-12-24 00:10:55 -0600

    more passport changes

--- config/passport-config.js
+++ config/passport-config.js
@@ -7,8 +7,8 @@ module.exports = passport => {
     done(null, user.uuid);
   });
 
-  passport.deserializeUser(function(uuid, done) {
-    db.Accounts.findById(uuid).then(function(user) {
+  passport.deserializeUser((uuid, done) => {
+    db.Accounts.findById(uuid).then(user => {
       if (user) {
         done(null, user.get());
       } else {
@@ -17,3 +17,50 @@ module.exports = passport => {
     });
   });
 };
+
+passport.use(
+  "local-signup",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "account_key",
+      passReqToCallback: true
+    },
+    (req, email, account_key, done) => {
+      process.nextTick(() => {
+        db.Accounts.findOne({
+          where: {
+            email
+          }
+        }).then((user, err) => {
+          if (err) {
+            console.log("err", err);
+            return done(err);
+          }
+          if (user) {
+            console.log("signupMessage", "That email is already taken.");
+            return done(
+              null,
+              false,
+              req.flash("signupMessage", "That email is already taken.")
+            );
+          } else {
+            db.user
+              .create({
+                username: req.body.username,
+                email: req.body.email,
+                address: req.body.address,
+                account_key: db.Accounts.generateHash(account_key)
+              })
+              .then(dbUser => {
+                return done(null, dbUser);
+              })
+              .catch(err => {
+                console.log(err);
+              });
+          }
+        });
+      });
+    }
+  )
+);
