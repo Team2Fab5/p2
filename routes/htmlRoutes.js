1f9005143affc8371f3c6a4b75fa7959734664c3
adam bindert <adamcbindert@gmail.com>
2018-12-29 16:40:53 -0600

    routes, posts, creates for each database

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,7 @@ var db = require("../models");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
+    //for user 
     db.user.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
@@ -10,6 +11,56 @@ module.exports = function (app) {
       });
 
     });
+
+    //for score 
+    db.score.findAll({}).then(function (dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+
+    });
+    //for requested task db 
+    db.requested_task.findAll({}).then(function (dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+
+    });
+
+    //for address 
+    db.address.findAll({}).then(function (dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+
+    });
+
+
+    //for completed tasks
+    db.completed_task.findAll({}).then(function (dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+
+    });
+
+
+
+    //for type 
+    db.type.findAll({}).then(function (dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+
+    });
+
+
+
   });
 
   // Load example page and pass in an example by id
@@ -21,7 +72,52 @@ module.exports = function (app) {
         example: dbExample
       });
     });
+
+    db.scores.findOne({ where: { id: req.params.id } }).then(function (
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+
+
+    db.requested_task.findOne({ where: { id: req.params.id } }).then(function (
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+
+    db.address.findOne({ where: { id: req.params.id } }).then(function (
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+
+
+    db.completed_task.findOne({ where: { id: req.params.id } }).then(function (
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+
+    db.type.findOne({ where: { id: req.params.id } }).then(function (
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+
+
   });
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
