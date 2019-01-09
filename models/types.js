236470a648ffe07ab0a348a53d14ee4c887eaabd
carnitas <lindsey.cardenas@gmail.com>
2019-01-08 20:29:06 -0600

    erging changes

--- models/types.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    var type = sequelize.define("type", {
-      taskType: DataTypes.INTEGER,
-      taskTitle: DataTypes.STRING,
-      taskDescr: DataTypes.STRING,
-      taskPoints: DataTypes.INTEGER
-    });
-    return type;
-  };
